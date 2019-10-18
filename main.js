51d7cd876bf55c0108e1650407283a378a695f8c
Baraabd <bara.abd@yahoo.com>
2019-10-18 09:15:47 +0200

    pusha

--- main.js
+++ main.js
@@ -12,16 +12,90 @@ function loadProducts() {
         });
 }
 
-
 function initSite() {
     loadProducts();
+    var body = document.getElementsByTagName("body")[0]
+    var container = document.createElement("div")
+    container.classList = "container"
+
     // This would also be a good place to initialize other parts of the UI
 }
 
 /** Uses the loaded products data to create a visible product list on the website */
+
 function addProductsToWebpage() {
     // Check your console to see that the products are stored in the listOfProducts varible.
-    console.log(listOfProducts);
+
+    var body = document.getElementsByTagName("body")[0]
+    var container = document.createElement("section")
+    var shopItems = document.createElement("div")
+    container.classList = "container" + " " + "content-section"
+    shopItems.classList = "shop-items"
+
+
+    for (var i = 0; i < listOfProducts.length; i++) {
+
+        var shopItem = document.createElement("div")
+        shopItem.classList = "shop-item"
+
+        var shopItemTitle = document.createElement("span")
+        shopItemTitle.classList = "shop-item-title"
+
+        var shopItemDescription = document.createElement("p")
+        shopItemDescription.classList = "shop-item-description"
+
+
+        var shopItemImage = document.createElement("IMG")
+        shopItemImage.classList = "shop-item-image"
+        shopItemImage.setAttribute("src", "./assets/" + listOfProducts[i].image)
+        shopItemImage.setAttribute("width", "200")
+        shopItemImage.setAttribute("height", "320")
+        shopItemImage.setAttribute("alt", "The Pulpit Rock")
+
+
+        var shopItemDetails = document.createElement("div")
+        shopItemDetails.classList = "shop-item-details"
+
+
+        var shopItemPrice = document.createElement("span")
+        shopItemPrice.classList = "shop-item-price"
+
+
+        var shopItemButton = document.createElement("button")
+        shopItemButton.innerText = "Lägg till i kundvagn"
+        shopItemButton.classList = "btn" + " " + "btn-primary" + " " + "shop-item-button"
+
+
+
+        shopItemTitle.innerText = listOfProducts[i].title
+        shopItemDescription.innerText = listOfProducts[i].description
+        shopItemImage.innerText = "./images" + listOfProducts[i].image
+        shopItemPrice.innerText = listOfProducts[i].price
+
+
+
+
+
+        shopItem.appendChild(shopItemTitle)
+        shopItem.appendChild(shopItemDescription)
+        shopItem.appendChild(shopItemImage)
+        shopItem.appendChild(shopItemDetails)
+
+
+
+
+
+        shopItemDetails.appendChild(shopItemPrice)
+        shopItemDetails.appendChild(shopItemButton)
+
+
+        shopItems.appendChild(shopItem)
+
+
+    }
+    container.appendChild(shopItems)
+    body.appendChild(container)
+
 
     // Add your code here, remember to brake your code in to smaller function blocks
     // to reduce complexity and increase readability. Each function should have
